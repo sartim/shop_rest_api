aa558868eb067c241d405556f48fad0267cdbac2
Tim <sarrtim@gmail.com>
2019-04-03 17:28:18 +0300

    Add dotenv loader

--- app/asgi.py
+++ app/asgi.py
@@ -1,7 +1,10 @@
 import os
 import django
+import dotenv
+
 from channels.routing import get_default_application
 
+dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
 django.setup()
 application = get_default_application()
