783f9f4efe609e9ecf6a76eec2d643375a8cc932
Tim <sarrtim@gmail.com>
2019-04-04 12:42:00 +0300

    Add CategoryAdmin

--- category/admin.py
+++ category/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from category.models import Category
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'name': ('name', 'slug')}
+    list_display = ('name', 'slug')
+    search_fields = ['name', 'slug']
+
+
+admin.site.register(Category, CategoryAdmin)
