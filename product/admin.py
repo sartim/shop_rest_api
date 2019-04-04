2e53424eb84a94da2fb4fd66fc789da424164141
Tim <sarrtim@gmail.com>
2019-04-04 12:41:30 +0300

    Add ProductAdmin

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from product.models import Product
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'name': ('name', 'slug')}
+    list_display = ('category', 'name', 'slug', 'image', 'description', 'price', 'stock', 'available', 'created',
+                    'updated')
+    search_fields = ['name', 'slug']
+
+
+admin.site.register(Product, ProductAdmin)
