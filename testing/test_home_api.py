232996130dd4e1c13dc843f558546192f51160dd
Tim <sarrtim@gmail.com>
2019-04-05 10:31:02 +0300

    Update TestHomeApi test case

--- testing/test_home_api.py
+++ testing/test_home_api.py
@@ -1,11 +1,8 @@
-from django.test import Client
-from django.urls import reverse
 from testing.base import Base
 
 
 class TestHomeApi(Base):
     def test_home_api(self):
-        client = Client()
-        r = client.get(reverse('home', kwargs={}))
+        r = self.client.get('/')
         assert r.status_code == 200
         assert r.data == "Welcome!"
