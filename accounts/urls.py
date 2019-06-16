0b226222028291712cb81e00a05aa45fbe33be9c
Tim <sarrtim@gmail.com>
2019-06-16 17:10:43 +0300

    Update urls to match package name

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,9 +3,9 @@ from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify
 from accounts.api import fetch_user_api, register_api
 
 urlpatterns = [
-    url(r'^api/v1/auth/token-auth/', obtain_jwt_token),
-    url(r'^api/v1/auth/token-refresh/', refresh_jwt_token),
-    url(r'^api/v1/auth/token-verify/', verify_jwt_token),
-    url(r'^api/v1/auth/register/', register_api.register, name='register-user'),
-    url(r'^api/v1/auth/user/$', fetch_user_api.fetch_user, name='fetch-user'),
+    url(r'^api/v1/account/token-auth/', obtain_jwt_token),
+    url(r'^api/v1/account/token-refresh/', refresh_jwt_token),
+    url(r'^api/v1/account/token-verify/', verify_jwt_token),
+    url(r'^api/v1/account/register/', register_api.register, name='register-user'),
+    url(r'^api/v1/account/user/$', fetch_user_api.fetch_user, name='fetch-user'),
 ]
