46124b626bf98b79096ca3fd774d593c94ec2131
Tim <sarrtim@gmail.com>
2019-06-16 17:14:19 +0300

    Add test case for retrieving product by id

--- tests/test_product_api.py
+++ tests/test_product_api.py
@@ -3,5 +3,9 @@ from tests.base import Base
 
 class TestProduct(Base):
     def test_get_all(self):
-        r = self.client.get('/api/v1/product/')
+        r = self.client.get(self.product_url)
+        assert r.status_code == 200
+
+    def test_get_by_id(self):
+        r = self.client.get('{}1'.format(self.product_url))
         assert r.status_code == 200
