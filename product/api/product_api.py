9b388e9353c3782ae7e807e0dad1ce0c1cf932c1
Tim <sarrtim@gmail.com>
2019-06-16 20:48:28 +0300

    Add product_api module

--- product/api/product_api.py
+++ product/api/product_api.py
@@ -1,5 +1,16 @@
 from django.conf.urls import url
-from product.views import ProductViewSet
+from rest_framework import viewsets
+from product.serializers import ProductSerializer
+from product.models import Product
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    serializer_class = ProductSerializer
+
+    def get_queryset(self, *args, **kwargs):
+        queryset = Product.objects.all()
+        return queryset
+
 
 product_list = ProductViewSet.as_view({'get': 'list', 'post': 'create'})
 product_detail = ProductViewSet.as_view({'get': 'retrieve', 'put': 'update', 'patch': 'partial_update',
