5b3b9b2f8315d13a3255e0f627d6df0c0605f2fa
Tim <sarrtim@gmail.com>
2023-10-31 18:23:15 +0300

    Update to use dotenv loader

--- tests/base.py
+++ tests/base.py
@@ -1,8 +1,11 @@
 import os
-import dotenv
+
+from dotenv import load_dotenv
 from django.core.wsgi import get_wsgi_application
 
-dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
+load_dotenv()
+
+# dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
 application = get_wsgi_application()
 
@@ -13,10 +16,10 @@ class Base:
     @classmethod
     def setup_class(cls):
         cls.client = APIClient()
-        cls.product_url = '/api/v1/product/'
-        cls.user_url = '/api/v1/account/user/'
-        cls.category_url = '/api/v1/category/'
-        cls.order_url = '/api/v1/order/'
+        cls.product_url = '/api/v1/products/'
+        cls.user_url = '/api/v1/account/users/'
+        cls.category_url = '/api/v1/categories/'
+        cls.order_url = '/api/v1/orders/'
         os.system('python manage.py migrate')
         os.system('python manage.py add_product_data')
 
