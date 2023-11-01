25735e55941e23e899acd26cc52e8902f7ffb869
Tim <sarrtim@gmail.com>
2023-11-01 11:13:39 +0300

    Update dotenv loader

--- app/wsgi.py
+++ app/wsgi.py
@@ -1,18 +1,9 @@
-"""
-WSGI config for app project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
-"""
-
 import os
-import dotenv
 
 from django.core.wsgi import get_wsgi_application
+from dotenv import load_dotenv
 
-dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
+load_dotenv()
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
 
 application = get_wsgi_application()
