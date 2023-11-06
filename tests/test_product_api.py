be6a32df42749b7d55b03eff615e25e94d94eb7b
Tim <sarrtim@gmail.com>
2023-11-07 00:59:39 +0300

    Fix and skip tests to be done

--- tests/test_product_api.py
+++ tests/test_product_api.py
@@ -1,11 +1,28 @@
+import unittest
+
+from rest_framework.test import APIClient
 from tests.base import Base
 
 
 class TestProduct(Base):
     def test_get_all(self):
-        r = self.client.get(self.product_url)
-        assert r.status_code == 401  # TODO assert for 200
+        self.client = APIClient()
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + str(self.token))
+        response = self.client.get(self.product_url, format='json')
+        assert response.status_code == 200
+        print(response.data)
 
+    @unittest.skip("TODO")
     def test_get_by_id(self):
-        r = self.client.get('{}1'.format(self.product_url))
-        assert r.status_code == 301  # TODO assert for 200
+        self.client = APIClient()
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + str(self.token))
+        response = self.client.get('{}1'.format(self.product_url), format='json')
+        assert response.status_code == 200
+
+    @unittest.skip("TODO")
+    def test_post(self):
+        self.client = APIClient()
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + str(self.token))
+        data = {}
+        response = self.client.post(self.product_url, data=data, format='json')
+        assert response.status_code == 201
